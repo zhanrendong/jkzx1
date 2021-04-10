99593a0bfa6be9f7a3a7a8cbaef6d1d11f568eb2
Baolin <b.l.shao84@gmail.com>
2021-04-10 09:28:06 +0800

    Update regression.config.js

--- scripts/regression.config.js
+++ scripts/regression.config.js
@@ -2,14 +2,14 @@ module.exports = {
     apps: [{
         name: 'bct-server',
         script: 'java',
-        args: ['-jar', '-Xms128m', '-Xmx8g', 'bin/bct-server-3.0.jar'],
+        args: ['-jar', '-Xms128m', '-Xmx8g', '-Djava.net.preferIPv4Stack=true', 'bin/bct-server-3.0.jar'],
         env: {
             FILE_LOCATION: 'document-service/'
         }
     }, {
         name: 'zuul-server',
         script: 'java',
-        args: ['-jar', '-Xms128m', '-Xmx2g', 'bin/zuul-server-3.0.jar']
+        args: ['-jar', '-Xms128m', '-Xmx2g', '-Djava.net.preferIPv4Stack=true','bin/zuul-server-3.0.jar']
     }, {
         name: 'ironman',
         script: 'python3',
